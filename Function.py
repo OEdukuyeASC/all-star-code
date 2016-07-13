b763bb85243ca9b25d75695495777271acd3eb88
OEdukuyeASC <ogedukuye@gmail.com>
2016-07-13 15:09:41 -0400

    l

--- Function.py
+++ Function.py
@@ -1,22 +1,27 @@
 from Myro import *
 init ("sim")
-
+# Email roger@allstarcode.org for questions, boi
 def drawE():
     penDown()
     i = 0
-    while i < 140:
+    while i < 147:
       
         forward (1, .01)
         motors(-.1, .25, 0.4) 
         i = i + 1
 
-def drawO():
+def drawE():
     penDown()
     i = 0
-    while i < 360:
+    while i < 147:
+      
         forward (1, .01)
         motors(-.1, .25, 0.4) 
         i = i + 1
+
         
        
-drawO()
+drawE()
+penUp()
+forward (1.8,1)
+drawE()
